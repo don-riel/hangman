20dfa0e4921b20982874fbbfd144c26d952bee60
don-riel <62613405+don-riel@users.noreply.github.com>
2020-08-09 13:33:53 +0200

    Updates

---
+++ precache-manifest.320924ad0c12231f6ad4db2334e50d82.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a27343382dda2fe0005ce2949ebc9253",
+    "url": "/hangman/index.html"
+  },
+  {
+    "revision": "e98d56d01bd85e7a5e06",
+    "url": "/hangman/static/css/main.abf52c80.chunk.css"
+  },
+  {
+    "revision": "e10e3cd905a6aa1d8a72",
+    "url": "/hangman/static/js/2.6d6c787d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hangman/static/js/2.6d6c787d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e98d56d01bd85e7a5e06",
+    "url": "/hangman/static/js/main.4f09c482.chunk.js"
+  },
+  {
+    "revision": "c74c887a996c8a1a0aa3",
+    "url": "/hangman/static/js/runtime-main.b0255b79.js"
+  },
+  {
+    "revision": "28b231442e3db4c709cfc8c6338afa0f",
+    "url": "/hangman/static/media/complete.28b23144.png"
+  },
+  {
+    "revision": "66a929785e150bf8ad9264a2450f26ee",
+    "url": "/hangman/static/media/fifth.66a92978.jpg"
+  },
+  {
+    "revision": "88396dfacd7faf7ce34a86c6c7ca412a",
+    "url": "/hangman/static/media/fourth.88396dfa.jpg"
+  }
+]);
